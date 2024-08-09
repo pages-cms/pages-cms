76e85c602097d28ae32d19128f3d581054d28811
Ronan Berder <hunvreus@gmail.com>
2024-08-09 20:43:59 +0800

    Remove unnecessary space

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -9,8 +9,8 @@ export function SignIn() {
   return (
     <div className="h-screen p-4 md:p-6 flex justify-center items-center">
       <div className="sm:max-w-[340px] w-full text-center space-y-6">
-        <h1 className="text-xl lg:text-2xl font-semibold tracking-tight mb-6">Sign in to Pages CMS</h1>
-        <form action={handleGithubSignIn} className="space-y-2">
+        <h1 className="text-xl lg:text-2xl font-semibold tracking-tight">Sign in to Pages CMS</h1>
+        <form action={handleGithubSignIn}>
           <SubmitButton type="submit" className="w-full">
             <Github className="h-4 w-4 mr-2" />
             Sign in with GitHub
