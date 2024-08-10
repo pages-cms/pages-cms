61e3ff6d439d16e972c2772616632a9cca57ee3e
Ronan Berder <hunvreus@gmail.com>
2024-08-10 11:52:51 +0800

    Fixing turso config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
 import "@/db/envConfig";
 import { defineConfig } from "drizzle-kit";
- 
+
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./db/migrations",
   dialect: "sqlite",
-  driver: process.env.SQLITE_AUTH_TOKEN ? "turso" : undefined,
+  driver: (process.env.SQLITE_AUTH_TOKEN && process.env.SQLITE_AUTH_TOKEN !== "")
+    ? "turso"
+    : undefined,
   dbCredentials: {
     url: process.env.SQLITE_URL!,
     authToken: process.env.SQLITE_AUTH_TOKEN,
-  },
+  }
 });
