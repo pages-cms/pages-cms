2352c2e51bd56992020dbe2eadb86b76a6d42dbc
Ryan Gittings <ryan@gittings.studio>
2025-03-12 12:00:17 +0000

    Add blocker

--- components/entry/entry-form.tsx
+++ components/entry/entry-form.tsx
@@ -56,6 +56,7 @@ import {
 } from "@dnd-kit/modifiers";
 import { CSS } from "@dnd-kit/utilities";
 import { ChevronLeft, GripVertical, Loader, Plus, Trash2 } from "lucide-react";
+import { Blocker } from "../navigation-block";
 
 const SortableItem = ({
   id,
@@ -321,6 +322,7 @@ const EntryForm = ({
 
   return (
     <Form {...form}>
+      {isDirty && <Blocker />}
       <form onSubmit={form.handleSubmit(handleSubmit)}>
         <div className="max-w-screen-xl mx-auto flex w-full gap-x-8">
           <div className="flex-1 w-0">
