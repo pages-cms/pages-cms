a7fe280d3851ee6e2ae87a02aa373e7f9468255a
Ryan Gittings <ryan@gittings.studio>
2025-03-13 09:50:53 +0000

    Draft alt text

---
+++ fields/core/rich-text/custom-image/image.tsx
@@ -0,0 +1,78 @@
+import Image from '@tiptap/extension-image'
+import { NodeViewWrapper, NodeViewWrapperProps, ReactNodeViewRenderer } from '@tiptap/react';
+import { useState } from 'react';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
+
+function ImageNode(props: NodeViewWrapperProps) {
+  const { updateAttributes } = props
+  const { src, alt } = props.node.attrs
+  const [isOpen, setIsOpen] = useState(false);
+
+  let className = 'image'
+  if (props.selected) { className += ' ProseMirror-selectednode' }
+
+  const handleSave = () => {
+    updateAttributes({ alt: altText });
+    setIsOpen(false);
+  }
+
+  return (
+    <NodeViewWrapper className={className} data-drag-handle>
+      <div className="relative">
+        <img src={src} alt={alt} />
+
+        <Popover open={isOpen} onOpenChange={setIsOpen}>
+          <PopoverTrigger asChild>
+            <Button className="absolute top-2 right-2" variant="secondary" size="xs">Edit</Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="sm:max-w-md">
+            <div className="grid gap-4">
+              <div className="space-y-2">
+                <h4 className="font-medium leading-none">Image</h4>
+                <p className="text-sm text-muted-foreground">
+                  Configure the image.
+                </p>
+              </div>
+              <div className="grid gap-2">
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="width">Alt Text</Label>
+                  <Input
+                    id="alt-text"
+                    value={alt}
+                    defaultValue="100%"
+                    className="col-span-2 h-8"
+                    onChange={(e) => updateAttributes({ alt: e.target.value })}
+                  />
+                </div>
+              </div>
+
+              <Button
+                variant="secondary"
+                size="xs"
+                onClick={() => setIsOpen(false)}
+              >
+                Close
+              </Button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </NodeViewWrapper>
+  )
+}
+
+export default Image.extend({
+  addNodeView() {
+    return ReactNodeViewRenderer(ImageNode)
+  }
+})
