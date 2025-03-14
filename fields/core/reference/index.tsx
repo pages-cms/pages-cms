a27934efdf2914afbe2a424c619dd4a4cd666d4a
Ronan Berder <hunvreus@gmail.com>
2025-03-14 16:26:36 +0800

    Fixed validation for reference

--- fields/core/reference/index.tsx
+++ fields/core/reference/index.tsx
@@ -1,3 +1,15 @@
+import { z } from "zod";
+import { Field } from "@/types/field";
 import { EditComponent } from "./edit-component";
 
-export { EditComponent };
+const schema = (field: Field) => {
+  let zodSchema: z.ZodTypeAny = z.coerce.string();
+
+  if (field.options?.multiple) zodSchema = z.array(zodSchema);
+  
+  if (!field.required) zodSchema = zodSchema.optional();
+  
+  return zodSchema;
+};
+
+export { schema, EditComponent };
