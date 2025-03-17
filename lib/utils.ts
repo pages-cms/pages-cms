b3d53888b9c5cd58002fde0f08d13191de5ae443
Ryan Gittings <ryan@gittings.studio>
2025-03-17 10:11:39 +0000

    Remove unnecessary code

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,10 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function isObject(item: any) {
-  return (item && typeof item === 'object' && !Array.isArray(item));
-}
-
 export function mergeDeep(...objects: any[]) {
   return merge({}, ...objects);
 }
