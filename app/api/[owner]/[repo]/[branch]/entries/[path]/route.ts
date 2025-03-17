b03566d84f906239709ab481b0e821bcbd439778
Ryan Gittings <ryan@gittings.studio>
2025-03-17 09:29:58 +0000

    Fix formatting

--- app/api/[owner]/[repo]/[branch]/entries/[path]/route.ts
+++ app/api/[owner]/[repo]/[branch]/entries/[path]/route.ts
@@ -1,8 +1,23 @@
 import { type NextRequest } from "next/server";
+import { createOctokitInstance } from "@/lib/utils/octokit";
+import { readFns } from "@/fields/registry";
+import { deepMap, getSchemaByName } from "@/lib/schema";
+import { parse } from "@/lib/serialization";
+import { getConfig } from "@/lib/utils/config";
+import { getFileExtension, normalizePath } from "@/lib/utils/file";
 import { getAuth } from "@/lib/auth";
 import { getToken } from "@/lib/token";
-import { getEntry } from "@/lib/utils/entry";
-import { normalizePath } from "@/lib/utils/file";
+import { getEntry } from "@/lib/entry";
+
+/**
+ * Fetches and parses individual file contents from GitHub repositories
+ * (usually for editing).
+ * 
+ * GET /api/[owner]/[repo]/[branch]/entries/[path]?name=[schemaName]
+ * 
+ * Requires authentication. If no schema name is provided, we return the raw
+ * contents.
+ */
 
 export async function GET(
   request: NextRequest,
@@ -12,8 +27,12 @@ export async function GET(
     const { user, session } = await getAuth();
     if (!session) return new Response(null, { status: 401 });
 
+    const token = await getToken(user, params.owner, params.repo);
+    if (!token) throw new Error("Token not found");
+
     const searchParams = request.nextUrl.searchParams;
-    const name = searchParams.get("name") || undefined; // Change null to undefined
+    const name = searchParams.get("name");
+    
     const normalizedPath = normalizePath(params.path);
 
     if (!name && normalizedPath !== ".pages.yml") throw new Error("If no content entry name is provided, the path must be \".pages.yml\".");
@@ -23,9 +42,9 @@ export async function GET(
     return Response.json({
       status: "success",
       data: {
-        sha: response.sha,
-        name: response.name,
-        path: response.path,
+        sha: response.data.sha,
+        name: response.data.name,
+        path: response.data.path,
         contentObject: response.contentObject
       }
     });
@@ -37,3 +56,52 @@ export async function GET(
     });
   }
 }
+
+// Parse the entry into an object based on the content schema.
+const parseContent = (
+  content: string,
+  schema: Record<string, any>,
+  config: Record<string, any>
+) => {
+  const serializedTypes = ["yaml-frontmatter", "json-frontmatter", "toml-frontmatter", "yaml", "json", "toml"];
+  
+  let contentObject: Record<string, any> = {};
+
+  if (serializedTypes.includes(schema && schema.format) && schema.fields && schema.fields.length > 0) {
+    // If we are dealing with a serialized format and we have fields defined
+    try {
+      contentObject = parse(content, { format: schema.format, delimiters: schema.delimiters });
+      // We resort to the same trick as with the client, wrapping things in a listWrapper object if we're dealing with a list at the root
+      let entryFields;
+      if (schema.list) {
+        contentObject = { listWrapper: contentObject };
+        entryFields = [{
+          name: "listWrapper",
+          type: "object",
+          list: true,
+          fields: schema.fields
+        }]
+      } else {
+        entryFields = schema.fields;
+      }
+
+      contentObject = deepMap(
+        contentObject,
+        entryFields,
+        (value, field) => {
+          if (field.hidden) return;
+          return readFns[field.type]
+            ? readFns[field.type](value, field,  config)
+            : value;
+        }
+      );
+      if (schema.list) contentObject = contentObject.listWrapper;
+    } catch (error: any) {
+      throw new Error(`Error parsing frontmatter: ${error.message}`);
+    }
+  } else {
+    contentObject = { body: content };
+  }
+  
+  return contentObject; 
+};
