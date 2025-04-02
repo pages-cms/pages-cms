7a0703cebea4ef737cadd32fbd62c0bc48d22132
Snow@Moana <snow@hol.ly>
2025-04-03 07:43:33 +1100

    WIP Add page titles.

---
+++ app/(main)/[owner]/[repo]/[branch]/collection/[name]/edit/[path]/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import Default from "./page";
+import { type PageWithPath } from "@/types/page";
+
+export async function generateMetadata(
+  { params }: PageWithPath,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: params.name,
+  };
+}
+
+export default function PageLayout(params: PageWithPath) {
+  return <Default {...params} />;
+}
